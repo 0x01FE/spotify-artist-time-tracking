364de4318fbaa3e42ba75d6450db19fcf19cbe1d
JISAUAY <jackson.d.hodge@jbhunt.com>
2023-06-30 09:30:44 -0500

    script now tracks overall time and time for specific days

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ from time import sleep
 from configparser import ConfigParser
 from spotipy.oauth2 import SpotifyOAuth
 from requests.exceptions import ConnectionError
+from datetime import datetime
 
 
-cache_path = "./data/.cache"
-json_path = "./data/music-time.json"
 
-wait_time = 90 # in seconds
 
 config = ConfigParser()
 config.read("config.ini")
@@ -18,13 +16,16 @@ client_id = config["SETTINGS"]["CLIENT_ID"]
 client_secret = config["SETTINGS"]["CLIENT_SECRET"]
 redirect_uri = config["SETTINGS"]["REDIRECT_URI"]
 scopes = config["SETTINGS"]["SCOPES"]
+wait_time = int(config["SETTINGS"]["WAIT_TIME"]) # in seconds
+cache_path = config["SETTINGS"]["CACHE_PATH"]
+json_path = config["SETTINGS"]["JSON_PATH"]
 
 os.environ["SPOTIPY_CLIENT_ID"] = client_id
 os.environ["SPOTIPY_CLIENT_SECRET"] = client_secret
 os.environ["SPOTIPY_REDIRECT_URI"] = redirect_uri
 
 
-with open("./data/.cache", "r") as f:
+with open(cache_path, "r") as f:
     token_info = json.loads(f.read())
 
 
@@ -34,25 +35,53 @@ spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scopes, cache_handler=
 
 
 
-# Returns dict of music-time.json
-def read() -> dict:
-    with open(json_path, "r") as f:
-        return json.loads(f.read())
-
-
-
 # Writes a dict to music-time.json
-def write(info : dict) -> None:
-    with open(json_path, "w") as f:
-        f.write(json.dumps(info, indent=4))
+def add_time(currently_playing : dict, filename : str) -> None:
+
+    if not os.path.exists(json_path + filename):
+        data = { "artists" : {}, "albums" : {}, "songs" : {} }
+    else:
+        with open(json_path + filename, "r") as f:
+            data = json.loads(f.read())
+
+    duration = currently_playing["item"]["duration_ms"]
+
+    # Add to artists listening time
+    for artist in currently_playing["item"]["artists"]:
+        if artist["name"] not in data["artists"]:
+            data["artists"][artist["name"]] = duration
+
+        else:
+            data["artists"][artist["name"]] += duration
+
+    # Add to album listen time
+    album = currently_playing["item"]["album"]["name"]
+
+    if album not in data["albums"]:
+        data["albums"][album] = duration
+
+    else:
+        data["albums"][album] += duration
+
+
+    # Add to song listen time
+    track_title = currently_playing["item"]["name"]
+
+    if track_title not in data["songs"]:
+        data["songs"][track_title] = duration
+
+    else:
+        data["songs"][track_title] += duration
+
+    with open(json_path + filename, "w") as f:
+        f.write(json.dumps(data, indent=4))
 
 
 
 def main() -> None:
-    last_progress = None
-    last_track_title = None
 
     while True:
+        print("#"*20)
         print("Looking for a playing song...")
         try:
             currently_playing = spotify.current_user_playing_track()
@@ -62,6 +91,12 @@ def main() -> None:
             continue
 
         add = False
+
+        with open("./data/last.json", "r") as f:
+            last_track_info = json.loads(f.read())
+
+        last_progress = last_track_info["last_progress"]
+        last_track_title = last_track_info["last_track_title"]
 
         # Series of checks to see if the program should actually consider this a "listen"
         if currently_playing:
@@ -76,50 +111,24 @@ def main() -> None:
 
         if add:
             print(f'Song detected, {currently_playing["item"]["name"]}')
-            data = read()
+            today_file = datetime.now().strftime("%d-%m-%Y") + ".json"
 
-            duration = currently_playing["item"]["duration_ms"]
+            for file in [today_file, "overall.json"]:
+                add_time(currently_playing, file)
 
-            # Add to artists listening time
-            for artist in currently_playing["item"]["artists"]:
-                if artist["name"] not in data["artists"]:
-                    data["artists"][artist["name"]] = duration
+            last_track_info["last_progress"] = currently_playing["item"]["duration_ms"]
+            last_track_info["last_track_title"] = currently_playing["item"]["name"]
 
-                else:
-                    data["artists"][artist["name"]] += duration
-
-
-            # Add to album listen time
-            album = currently_playing["item"]["album"]["name"]
-
-            if album not in data["albums"]:
-                data["albums"][album] = duration
-
-            else:
-                data["albums"][album] += duration
-
-
-            # Add to song listen time
-            track_title = currently_playing["item"]["name"]
-
-            if track_title not in data["songs"]:
-                data["songs"][track_title] = duration
-
-            else:
-                data["songs"][track_title] += duration
-
-            last_progress = duration
-            last_track_title = track_title
-
-            write(data)
+            with open("./data/last.json", "w") as f:
+                f.write(json.dumps(last_track_info, indent=4))
 
         else:
             print("Listening check not passed.")
 
         # Wait before checking again to avoid being rate limited or using my API quota
         print(f"Wating {wait_time} seconds...")
+        print("#"*20 + "\n")
         sleep(wait_time)
-
 
 
 
